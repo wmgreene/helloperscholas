4a5ec438a8c329d8be28db9f7fdb0ebf2d690858
wmgreene <wmoralgreene@gmail.com>
2023-04-17 13:57:52 -0400

    My first commit

---
+++ src/main/Helloworld.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Helloworld {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+	}
+	
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author wmgreen
+ *
+ */
+module hello {
+}
